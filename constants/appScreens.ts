9f2fc8deac0440dfa0b617f6aa6bdb576aa31397
Milza <marcomilza@gmail.com>
2025-05-31 02:53:05 +0200

    updated root

--- constants/appScreens.ts
+++
@@ -1,9 +0,0 @@
-enum appScreens {
-  START_GAME_SCREEN = 'START_GAME_SCREEN',
-  GAME_SCREEN = 'GAME_SCREEN',
-  GAME_OVER_SCREEN = 'GAME_OVER_SCREEN',
-}
-
-type ISetScreen = (str: appScreens) => void;
-
-export { appScreens, ISetScreen };
