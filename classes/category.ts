850257157038fff05d96175a5bf6e6a3d392730d
Milza <marcomilza@gmail.com>
2025-06-01 19:07:20 +0200

    Category updated

--- classes/category.ts
+++ classes/category.ts
@@ -1,9 +1,5 @@
 class Category {
-  constructor(id, title, color) {
-    this.id = id;
-    this.title = title;
-    this.color = color;
-  }
+  constructor(public id: string, public titie: string, public color: string) {}
 }
 
 export default Category;
