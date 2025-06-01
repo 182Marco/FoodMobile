b0f8220f61f7606c25445782fc9a88fbdbad0dec
Milza <marcomilza@gmail.com>
2025-06-01 19:31:12 +0200

    removed unused

--- components/screens/startGameScreen/startGameScreen.hook.ts
+++
@@ -1,9 +0,0 @@
-import * as R from 'react';
-import { IUseStartGameScreen } from './startGameScreen.models';
-import { useStyles } from './startGameScreen.style';
-
-const useStartGameScreen: IUseStartGameScreen = p => {
-  return {};
-};
-
-export { useStartGameScreen };
