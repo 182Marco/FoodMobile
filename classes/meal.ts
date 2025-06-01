0e8742c5eae4dac5dccfef8e35268f167f6eeda6
Milza <marcomilza@gmail.com>
2025-06-01 19:04:55 +0200

    meal updated

--- classes/meal.ts
+++ classes/meal.ts
@@ -1,33 +1,19 @@
 class Meal {
   constructor(
-    id,
-    categoryIds,
-    title,
-    affordability,
-    complexity,
-    imageUrl,
-    duration,
-    ingredients,
-    steps,
-    isGlutenFree,
-    isVegan,
-    isVegetarian,
-    isLactoseFree
-  ) {
-    this.id = id;
-    this.categoryIds = categoryIds;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.ingredients = ingredients;
-    this.steps = steps;
-    this.duration = duration;
-    this.complexity = complexity;
-    this.affordability = affordability;
-    this.isGlutenFree = isGlutenFree;
-    this.isVegan = isVegan;
-    this.isVegetarian = isVegetarian;
-    this.isLactoseFree = isLactoseFree;
-  }
+    public id: string,
+    public categoryIds: string[],
+    public title: string,
+    public affordability: string,
+    public complexity: string,
+    public imageUrl: string,
+    public duration: number,
+    public ingredients: string[],
+    public steps: string[],
+    public isGlutenFree: boolean,
+    public isVegan: boolean,
+    public isVegetarian: boolean,
+    public isLactoseFree: boolean
+  ) {}
 }
 
-export default Meal;
+export { Meal };
