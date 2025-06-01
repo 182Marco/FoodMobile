038498a4cdf160a9072b9d8ac89d07ca35c069ce
Milza <marcomilza@gmail.com>
2025-06-01 19:32:22 +0200

    updated category logic

---
+++ components/screens/categoriesScreen/categoriesScreen.hook.ts
@@ -0,0 +1,9 @@
+import * as R from 'react';
+import { useStyles } from './categoriesScreen.style';
+import { IUseCategoriesScreen } from './categoriesScreen.models';
+
+const useCategoriesScreen: IUseCategoriesScreen = () => {
+  return { something: 2 };
+};
+
+export { useCategoriesScreen };
