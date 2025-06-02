1a562f471e27a710103aaebd03cf5d7cf97362bb
Milza <marcomilza@gmail.com>
2025-06-02 17:07:47 +0200

    updated root

--- components/Root/Root.tsx
+++ components/Root/Root.tsx
@@ -1,12 +1,27 @@
-import * as N from 'react-native';
-import { S } from './Root.style';
+import {} from 'expo';
 import { useRoot } from './Root.hook';
-import { diceImg } from '@/constants';
 import * as C from '@/components';
+import { StatusBar } from 'expo-status-bar';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
 
 const Root: React.FC = () => {
   const h = useRoot();
-  return <C.CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="dark" />
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="MealsCategories"
+            component={C.CategoriesScreen}
+          ></Stack.Screen>
+        </Stack.Navigator>
+      </NavigationContainer>
+    </>
+  );
 };
 
 export { Root };
