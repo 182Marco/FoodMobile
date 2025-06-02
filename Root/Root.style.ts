7413b1d697c4064495a82d94c687fe85f90e20b7
Milza <marcomilza@gmail.com>
2025-06-03 00:02:41 +0200

    updated root style

---
+++ Root/Root.style.ts
@@ -0,0 +1,17 @@
+import { colors } from '@/styles';
+import { StyleSheet } from 'react-native';
+
+const S = StyleSheet.create({
+  wrap: {
+    flex: 1,
+  },
+  bg: {
+    flex: 1,
+    backgroundColor: colors.primary,
+  },
+  img: {
+    opacity: 0.9,
+  },
+});
+
+export { S };
