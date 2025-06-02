475cef4d121d1886e60f6582ad58647888cceaab
Milza <marcomilza@gmail.com>
2025-06-02 15:33:43 +0200

    categoryGridTitle.style updated

--- components/screens/categoriesScreen/categoriesScreen.style.ts
+++ components/screens/categoriesScreen/categoriesScreen.style.ts
@@ -2,11 +2,16 @@ import { StyleSheet } from 'react-native';
 import * as N from 'react-native';
 import * as Gs from '@/styles';
 
-const useStyles = () => {
+const getStyles = () => {
   const { height } = N.useWindowDimensions();
   const IsLandskape = height < 380;
 
-  return StyleSheet.create({});
+  return StyleSheet.create({
+    list: {
+      backgroundColor: 'white',
+      flex: 1,
+    },
+  });
 };
 
-export { useStyles };
+export { getStyles };
