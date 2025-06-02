0a2d3320eeb5425af295c80505da5c93f43bd965
Milza <marcomilza@gmail.com>
2025-06-02 15:20:55 +0200

    categoryGridTitle.style updated

---
+++ components/atoms/categoryGridTitle/categoryGridTitle.style.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+import * as N from 'react-native';
+import * as Gs from '@/styles';
+
+const getStyles = () => {
+  const { height } = N.useWindowDimensions();
+  const IsLandskape = height < 380;
+
+  return StyleSheet.create({
+    gridItem: {
+      flex: 1,
+      margin: 16,
+      height: 150,
+      borderRadius: 8,
+      elevation: 4,
+    },
+
+    text: {
+      color: 'black',
+      backgroundColor: 'white',
+      flex: 1,
+    },
+  });
+};
+
+export { getStyles };
