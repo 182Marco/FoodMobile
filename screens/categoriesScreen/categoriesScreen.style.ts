d96d34f8b0a77aca297ad2ec71a2806443c875db
Milza <marcomilza@gmail.com>
2025-06-05 00:10:43 +0200

    updated category screen style

--- screens/categoriesScreen/categoriesScreen.style.ts
+++ screens/categoriesScreen/categoriesScreen.style.ts
@@ -1,6 +1,5 @@
 import { StyleSheet } from 'react-native';
 import * as N from 'react-native';
-import * as Gs from '@/styles';
 
 const getStyles = () => {
   const { height } = N.useWindowDimensions();
