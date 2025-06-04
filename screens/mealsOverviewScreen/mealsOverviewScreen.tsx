653ad90e8c57806b908677ae664351a44b345139
Milza <marcomilza@gmail.com>
2025-06-05 00:11:14 +0200

    updated overview meals  screen style

--- screens/mealsOverviewScreen/mealsOverviewScreen.tsx
+++ screens/mealsOverviewScreen/mealsOverviewScreen.tsx
@@ -1,11 +1,12 @@
 import * as N from 'react-native';
 import { useMealsOverviewScreen } from './mealsOverviewScreen.hook';
+import { MEALS } from '@/data';
 
 const mealsOverviewScreen = () => {
   const { S, ...h } = useMealsOverviewScreen();
   return (
     <N.View>
-      <N.Text>Meals Overview</N.Text>
+      <N.Text>{JSON.stringify(MEALS)}</N.Text>
     </N.View>
   );
 };
