9e27c31c6d2d33df0b1e4fe32b3f83fc0c9cf6e1
Milza <marcomilza@gmail.com>
2025-06-05 00:42:39 +0200

    updated logic

---
+++ screens/mealsOverviewScreen/mealsOverviewScreen.hook.ts
@@ -0,0 +1,9 @@
+import { getStyles } from './mealsOverviewScreen.style';
+
+const useMealsOverviewScreen = () => {
+  const S = getStyles();
+  const something = 'This is a placeholder for future functionality';
+  return { S, something };
+};
+
+export { useMealsOverviewScreen };
