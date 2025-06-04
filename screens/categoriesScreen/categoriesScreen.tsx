ff7e9277b1e870edc95092da89589f1dfac3de09
Milza <marcomilza@gmail.com>
2025-06-05 00:40:30 +0200

    added navigation

--- screens/categoriesScreen/categoriesScreen.tsx
+++ screens/categoriesScreen/categoriesScreen.tsx
@@ -6,7 +6,7 @@ import { CATEGORIES } from '@/data';
 import { useCategoriesScreen } from './categoriesScreen.hook';
 import { ICategoriesScreen } from './categoriesScreen.models';
 
-const CategoriesScreen: ICategoriesScreen = () => {
+const CategoriesScreen: ICategoriesScreen = ({ navigation }) => {
   const { S, ...h } = useCategoriesScreen();
   return (
     <>
@@ -15,7 +15,11 @@ const CategoriesScreen: ICategoriesScreen = () => {
         data={CATEGORIES}
         keyExtractor={e => e.id}
         renderItem={e => (
-          <C.CategoryGridTitle title={e.item.title} color={e.item.color} />
+          <C.CategoryGridTitle
+            onPress={() => navigation.navigate('MealsOverview')}
+            title={e.item.title}
+            color={e.item.color}
+          />
         )}
         numColumns={2}
       />
