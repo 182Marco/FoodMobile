2f480e197e6ab8b9f03b216734918969aeb1a6b8
Milza <marcomilza@gmail.com>
2025-06-05 00:11:42 +0200

    new screan for meals style

---
+++ screens/mealsOverviewScreen/mealsOverviewScreen.style.ts
@@ -0,0 +1,16 @@
+import { StyleSheet } from 'react-native';
+import * as N from 'react-native';
+import * as Gs from '@/styles';
+
+const getStyles = () => {
+  const { height } = N.useWindowDimensions();
+  const IsLandskape = height < 380;
+
+  return StyleSheet.create({
+    list: {
+      flex: 1,
+    },
+  });
+};
+
+export { getStyles };
