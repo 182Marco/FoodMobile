8bb48ceca0bae599b5399926a21d4c67daebbeef
Milza <marcomilza@gmail.com>
2025-06-05 01:12:42 +0200

    remouved unused

--- screens/mealsOverviewScreen/mealsOverviewScreen.hook.ts
+++
@@ -1,9 +0,0 @@
-import { getStyles } from './mealsOverviewScreen.style';
-
-const useMealsOverviewScreen = () => {
-  const S = getStyles();
-  const something = 'This is a placeholder for future functionality';
-  return { S, something };
-};
-
-export { useMealsOverviewScreen };
