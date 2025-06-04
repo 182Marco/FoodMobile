87597d8f44aeecd02ad6c0311d9fe99be04433d0
Milza <marcomilza@gmail.com>
2025-06-05 00:01:31 +0200

    new screan for meals

---
+++ screens/mealsOverviewScreen/mealsOverviewScreen.tsx
@@ -0,0 +1,10 @@
+const mealsOverviewScreen = () => {
+  return (
+    <div>
+      <h1>Meals Overview</h1>
+      <p>This is the meals overview screen.</p>
+    </div>
+  );
+};
+
+export { mealsOverviewScreen };
