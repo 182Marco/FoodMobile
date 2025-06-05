6cc7a9313c55da06f39ff489b297bc2c5ce18520
Milza <marcomilza@gmail.com>
2025-06-06 00:39:00 +0200

    navigation models updated

--- models/navigationModels.ts
+++ models/navigationModels.ts
@@ -1,10 +1,6 @@
-import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-
 type IRootStackParamList = {
   MealsCategories: undefined;
   MealsOverview: { categoryId: string };
 };
 
-type INavigation = NativeStackNavigationProp<Partial<IRootStackParamList>>;
-
-export { INavigation };
+export { IRootStackParamList };
--- screens/mealsOverwieScreen/mealsOverviewScreen.tsx
+++ screens/mealsOverwieScreen/mealsOverviewScreen.tsx
@@ -1,14 +1,23 @@
 import * as N from 'react-native';
 import { useMealsOverviewScreen } from './mealsOverviewScreen.hook';
 import { MEALS } from '@/data';
+import { IMealsOverviewScreen } from './mealsOverview.models';
 
-const mealsOverviewScreen = () => {
-  const { S, ...h } = useMealsOverviewScreen();
+const MealsOverviewScreen: IMealsOverviewScreen = ({ navigation, route }) => {
+  const { S, ...h } = useMealsOverviewScreen(navigation, route);
   return (
     <N.View style={S.wrap}>
-      <N.Text>{JSON.stringify(MEALS)}</N.Text>
+      <N.Text style={{ color: 'red' }}>{route.params.categoryId}</N.Text>
+      {/*       <N.Text>{JSON.stringify(h.selectedCategory, null, 2)}</N.Text> */}
+      <N.Text style={{ color: 'green' }}>
+        {JSON.stringify(
+          h.selectedCategory.every((e: any) =>
+            e.categoryIds.every((el: any) => el === route.params.categoryId)
+          )
+        )}
+      </N.Text>
     </N.View>
   );
 };
 
-export { mealsOverviewScreen };
+export { MealsOverviewScreen };
