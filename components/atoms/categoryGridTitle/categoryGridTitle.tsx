c26eb1a3bfb81b08dd9645b839f2d4698aa775c6
Milza <marcomilza@gmail.com>
2025-06-06 00:36:22 +0200

    grid title  updated

--- components/atoms/categoryGridTitle/categoryGridTitle.tsx
+++ components/atoms/categoryGridTitle/categoryGridTitle.tsx
@@ -5,11 +5,12 @@ import { pressedBtnEffects } from './categoryGridTitle.style';
 
 const CategoryGridTitle: ICategoryGridTitle = p => {
   const { S, ...h } = useCategoryGridTitle(p);
+
   return (
     <N.View style={S.gridItem}>
       <N.Pressable
         {...pressedBtnEffects(p)}
-        onPress={() => h.navigate('MealsOverview')}
+        onPress={() => h.navigate('MealsOverview', { categoryId: p.id })}
       >
         <N.View style={S.innerWrap}>
           <N.Text style={S.text}>{p.title}</N.Text>
