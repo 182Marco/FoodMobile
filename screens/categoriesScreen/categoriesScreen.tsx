323aeb6a729728d5b59dd81a07a6c80ab1e7f4a2
Milza <marcomilza@gmail.com>
2025-06-06 00:37:25 +0200

    category screen updated

--- screens/categoriesScreen/categoriesScreen.tsx
+++ screens/categoriesScreen/categoriesScreen.tsx
@@ -1,22 +1,18 @@
 import * as N from 'react-native';
 import * as C from '@/components';
-import * as Gs from '@/styles';
-import { texts } from '@/texts';
 import { CATEGORIES } from '@/data';
 import { useCategoriesScreen } from './categoriesScreen.hook';
 import { ICategoriesScreen } from './categoriesScreen.models';
 
-const CategoriesScreen: ICategoriesScreen = () => {
-  const { S, ...h } = useCategoriesScreen();
+const CategoriesScreen: ICategoriesScreen = ({ route }) => {
+  const { S, ...h } = useCategoriesScreen(route);
   return (
     <>
       <N.FlatList
         style={S.list}
         data={CATEGORIES}
         keyExtractor={e => e.id}
-        renderItem={e => (
-          <C.CategoryGridTitle title={e.item.title} color={e.item.color} />
-        )}
+        renderItem={e => <C.CategoryGridTitle {...e.item} />}
         numColumns={2}
       />
     </>
