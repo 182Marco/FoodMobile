4c1a37fd0c1f8dc6f30a33300609c09661c4641f
Milza <marcomilza@gmail.com>
2025-06-06 00:43:21 +0200

    remouved unused

--- components/atoms/categoryGridTitle/categoryGridTitle.hook.ts
+++ components/atoms/categoryGridTitle/categoryGridTitle.hook.ts
@@ -2,12 +2,11 @@ import * as R from 'react';
 import { getStyles } from './categoryGridTitle.style';
 import { IPassCategoryGridTitleProp } from './categortGridTitle.models';
 import { useNavigation } from '@react-navigation/native';
-import { INavigation } from '@/models';
 
 const useCategoryGridTitle: IPassCategoryGridTitleProp = p => {
   const S = getStyles(p);
 
-  const { navigate } = useNavigation<INavigation>();
+  const { navigate } = useNavigation();
 
   return { S, navigate };
 };
