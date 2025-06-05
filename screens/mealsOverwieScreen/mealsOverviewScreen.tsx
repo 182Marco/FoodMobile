03a4fadf208a4ecc7a19ebd03cc17978d715b42b
Milza <marcomilza@gmail.com>
2025-06-06 01:09:15 +0200

    mealsOverview  updated

--- screens/mealsOverwieScreen/mealsOverviewScreen.tsx
+++ screens/mealsOverwieScreen/mealsOverviewScreen.tsx
@@ -1,21 +1,17 @@
 import * as N from 'react-native';
+import * as C from '@/components';
 import { useMealsOverviewScreen } from './mealsOverviewScreen.hook';
-import { MEALS } from '@/data';
 import { IMealsOverviewScreen } from './mealsOverview.models';
 
 const MealsOverviewScreen: IMealsOverviewScreen = ({ navigation, route }) => {
   const { S, ...h } = useMealsOverviewScreen(navigation, route);
   return (
     <N.View style={S.wrap}>
-      <N.Text style={{ color: 'red' }}>{route.params.categoryId}</N.Text>
-      {/*       <N.Text>{JSON.stringify(h.selectedCategory, null, 2)}</N.Text> */}
-      <N.Text style={{ color: 'green' }}>
-        {JSON.stringify(
-          h.selectedCategory.every((e: any) =>
-            e.categoryIds.every((el: any) => el === route.params.categoryId)
-          )
-        )}
-      </N.Text>
+      <N.FlatList
+        data={h.selectedCategory}
+        keyExtractor={e => e.id}
+        renderItem={e => <C.RenderMeal {...e.item} />}
+      />
     </N.View>
   );
 };
