afa7d17784848b709c078a6fcb5add9f24af1cd9
Milza <marcomilza@gmail.com>
2025-06-07 00:06:14 +0200

    colors updated

--- styles/colors.ts
+++ styles/colors.ts
@@ -3,18 +3,8 @@ import { IAndroid_ripple } from './models';
 import { isHexColor } from './utils';
 
 const colors: Record<string, CSSColor> = {
-  btnPrimaryRipple: '#640233',
-  primaryBtn: '#73183F',
-  secondary: '#4e0329',
-  primary: '#ddb52f',
-  inputBackground: '#334155',
-  tertiary: '#64748B',
-  textColor: '#F1F5F9',
-  errorColor: '#F87171',
-  buttonColor: '#3B82F6',
-  goalTextColor: '#E2E8F0',
-  goalBorderColor: '#475569',
-  checked: '#00FF00',
+  headerBg: '#351401',
+  bg: '#3f2f25',
 };
 
 const android_ripple: IAndroid_ripple = color => ({
