d72956ce55482c5c0837bd45edc309471f0902db
Milza <marcomilza@gmail.com>
2025-06-06 13:22:45 +0200

    added style

---
+++ components/atoms/renderMeal/renderMeal.style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from 'react-native';
+import * as Gs from '@/styles';
+import { title } from 'process';
+
+const S = StyleSheet.create({
+  img: {
+    width: '100%',
+    height: 200,
+  },
+});
+
+export { S };
