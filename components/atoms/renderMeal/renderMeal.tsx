00f072411244cc1b63240920cd5ce21c9f829514
Milza <marcomilza@gmail.com>
2025-06-06 13:21:01 +0200

    added img

--- components/atoms/renderMeal/renderMeal.tsx
+++ components/atoms/renderMeal/renderMeal.tsx
@@ -1,11 +1,15 @@
 import * as N from 'react-native';
 import { IRenderMeal } from './renderMeal.models';
+import { S } from './renderMeal.style';
 
 const RenderMeal: IRenderMeal = p => {
   return (
     <N.View>
       <N.Pressable>
-        <N.Text>{p.title}</N.Text>
+        <N.View>
+          <N.Image style={S.img} source={{ uri: p.imageUrl }} />
+          <N.Text>{p.title}</N.Text>
+        </N.View>
       </N.Pressable>
     </N.View>
   );
