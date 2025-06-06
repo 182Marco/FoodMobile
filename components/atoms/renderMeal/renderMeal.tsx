aeeb72173ecac9cf90edf27ba190af09c6a7f294
Milza <marcomilza@gmail.com>
2025-06-07 00:09:21 +0200

    renderMeal updated

--- components/atoms/renderMeal/renderMeal.tsx
+++ components/atoms/renderMeal/renderMeal.tsx
@@ -1,14 +1,21 @@
 import * as N from 'react-native';
 import { IRenderMeal } from './renderMeal.models';
-import { S } from './renderMeal.style';
+import { pressedEffect, S } from './renderMeal.style';
 
 const RenderMeal: IRenderMeal = p => {
   return (
-    <N.View>
-      <N.Pressable>
-        <N.View>
-          <N.Image style={S.img} source={{ uri: p.imageUrl }} />
-          <N.Text>{p.title}</N.Text>
+    <N.View style={S.wrap}>
+      <N.Pressable {...pressedEffect}>
+        <N.View style={S.innerWrap}>
+          <N.View>
+            <N.Image style={S.img} source={{ uri: p.imageUrl }} />
+            <N.Text style={S.title}>{p.title}</N.Text>
+          </N.View>
+          <N.View style={S.details}>
+            <N.Text style={S.detailEl}>{p.duration}m </N.Text>
+            <N.Text style={S.detailEl}>{p.complexity.toUpperCase()}</N.Text>
+            <N.Text style={S.detailEl}>{p.affordability.toUpperCase()}</N.Text>
+          </N.View>
         </N.View>
       </N.Pressable>
     </N.View>
