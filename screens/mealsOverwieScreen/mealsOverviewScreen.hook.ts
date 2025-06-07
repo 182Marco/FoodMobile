da4d026e6c368b000422b72f354206ea818b584b
Milza <marcomilza@gmail.com>
2025-06-07 15:51:23 +0200

    updated MealsOverviewScreen

--- screens/mealsOverwieScreen/mealsOverviewScreen.hook.ts
+++ screens/mealsOverwieScreen/mealsOverviewScreen.hook.ts
@@ -1,16 +1,20 @@
 import { MEALS } from '@/data';
 import { INavigation, IRoute } from './mealsOverview.models';
 import { getStyles } from './mealsOverviewScreen.style';
+import { getCategoryTitleById } from '@/utils';
+import * as R from 'react';
 
 const useMealsOverviewScreen = (navigation: INavigation, route: IRoute) => {
   const S = getStyles();
   const {
-    params: { categoryId },
+    params: { catId },
   } = route;
 
-  const selectedCategory = MEALS.filter(e =>
-    e.categoryIds.includes(categoryId)
-  );
+  const selectedCategory = MEALS.filter(e => e.categoryIds.includes(catId));
+
+  R.useEffect(() => {
+    navigation.setOptions(getCategoryTitleById(catId));
+  }, [catId, navigation]);
 
   return { S, selectedCategory };
 };
