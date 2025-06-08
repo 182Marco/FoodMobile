3c5704b4f5037a8c26b0903da93ce74b398db280
Milza <marcomilza@gmail.com>
2025-06-08 12:24:18 +0200

    updated gridTitle comp models

--- components/atoms/categoryGridTitle/categoryGridTitle.models.ts
+++ components/atoms/categoryGridTitle/categoryGridTitle.models.ts
@@ -1,9 +1,18 @@
-interface ICategoryGridTitleProps {
+import { ScreenNames } from '@/constants';
+import { IRootStackParamList } from '@/models';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
+
+interface IGridTitleProps {
   title: string;
   color: string;
   id: string;
 }
 
-type ICategoryGridTitle = React.FC<ICategoryGridTitleProps>;
+type ICategoryGridTitle = React.FC<IGridTitleProps>;
 
-export { ICategoryGridTitle, ICategoryGridTitleProps };
+type INavigationProp = NativeStackNavigationProp<
+  IRootStackParamList,
+  ScreenNames.mealsOverview
+>;
+
+export { ICategoryGridTitle, IGridTitleProps, INavigationProp };
