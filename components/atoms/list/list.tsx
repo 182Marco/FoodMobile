8431b2b22af022532c843a1f3e8d88153ea18f23
Milza <marcomilza@gmail.com>
2025-06-09 00:30:14 +0200

    created list comp

---
+++ components/atoms/list/list.tsx
@@ -0,0 +1,15 @@
+import * as N from 'react-native';
+import { IList } from './list.model';
+import { S } from './list.style';
+
+const List: IList = p => (
+  <>
+    {p.data.map((e, i) => (
+      <N.View style={S.wrap} key={i}>
+        <N.Text style={S.text}>{e}</N.Text>
+      </N.View>
+    ))}
+  </>
+);
+
+export { List };
