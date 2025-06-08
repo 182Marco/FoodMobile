6e81cdfeb8586f74e1b2feec47a5dc1919c812e0
Milza <marcomilza@gmail.com>
2025-06-08 12:21:45 +0200

    updated meal detail screen models

---
+++ screens/mealDetailsScreen/MealDetailsScreen.models.ts
@@ -0,0 +1,12 @@
+import { ScreenNames } from '@/constants';
+import { IRootStackParamList } from '@/models';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+
+type IMealsDetailsScreenProps = NativeStackScreenProps<
+  IRootStackParamList,
+  ScreenNames.mealsDetail
+>;
+
+type IMealsDetailsScreen = React.FC<IMealsDetailsScreenProps>;
+
+export { IMealsDetailsScreen };
