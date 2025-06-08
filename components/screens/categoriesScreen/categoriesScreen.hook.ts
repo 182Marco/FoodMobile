ca147999700168428e525634486a2ff7103fd14e
Milza <marcomilza@gmail.com>
2025-06-09 00:34:54 +0200

    removed unused

--- components/screens/categoriesScreen/categoriesScreen.hook.ts
+++
@@ -1,9 +0,0 @@
-import * as R from 'react';
-import { useStyles } from './categoriesScreen.style';
-import { IUseCategoriesScreen } from './categoriesScreen.models';
-
-const useCategoriesScreen: IUseCategoriesScreen = () => {
-  return { something: 2 };
-};
-
-export { useCategoriesScreen };
