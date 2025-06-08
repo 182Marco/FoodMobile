2c6890d1ad74f1dac90c5ad9517926de685b7b98
Milza <marcomilza@gmail.com>
2025-06-08 12:30:00 +0200

    updated mealsOverviewScreen screen models

--- screens/mealsOverwieScreen/mealsOverview.models.ts
+++ screens/mealsOverwieScreen/mealsOverview.models.ts
@@ -1,17 +1,19 @@
+import { ScreenNames } from '@/constants';
 import { IRootStackParamList } from '@/models';
 import { NavigationProp, RouteProp } from '@react-navigation/native';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 
 type IMealsOverviewScreenProps = NativeStackScreenProps<
   IRootStackParamList,
-  'MealsOverview'
+  ScreenNames.mealsOverview
 >;
 
 type IMealsOverviewScreen = React.FC<IMealsOverviewScreenProps>;
 
-type IUseMealsOverviewScreen = (
-  navigation: NavigationProp<IRootStackParamList, 'MealsOverview'>,
-  route: RouteProp<IRootStackParamList, 'MealsOverview'>
-) => Record<string, any>;
+type INavigation = NavigationProp<
+  IRootStackParamList,
+  ScreenNames.mealsOverview
+>;
+type IRoute = RouteProp<IRootStackParamList, ScreenNames.mealsOverview>;
 
-export { IMealsOverviewScreen, IUseMealsOverviewScreen };
+export { IMealsOverviewScreen, INavigation, IRoute };
