4364c71f3f5b4b2bebca61e9e425032462328046
Milza <marcomilza@gmail.com>
2025-06-08 12:31:31 +0200

    created screen costants

---
+++ constants/screens.ts
@@ -0,0 +1,13 @@
+enum ScreenTitles {
+  mealsCategories = 'Meals Categories',
+  mealsOverview = 'Meals Overview',
+  mealsDetail = 'Meal Details',
+}
+
+enum ScreenNames {
+  mealsCategories = 'MealsCategories',
+  mealsOverview = 'MealsOverview',
+  mealsDetail = 'MealDetails',
+}
+
+export { ScreenTitles, ScreenNames };
