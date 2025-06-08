ee356c7c83a78de11c1b989a40873efe8e86d209
Milza <marcomilza@gmail.com>
2025-06-08 12:28:26 +0200

    updated categoriesScreen logic

--- screens/categoriesScreen/categoriesScreen.hook.ts
+++ screens/categoriesScreen/categoriesScreen.hook.ts
@@ -1,9 +1,10 @@
 import { IRootStackParamList } from '@/models';
 import { getStyles } from './categoriesScreen.style';
 import { RouteProp } from '@react-navigation/native';
+import { ScreenNames } from '@/constants';
 
 const useCategoriesScreen = (
-  route: RouteProp<IRootStackParamList, 'MealsCategories'>
+  route: RouteProp<IRootStackParamList, ScreenNames.mealsCategories>
 ) => {
   const { params } = route;
   const S = getStyles();
