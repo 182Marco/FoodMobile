ca1d7a2beb1a8524681409163820b66bc0b76b03
Milza <marcomilza@gmail.com>
2025-06-09 00:29:56 +0200

    updated render model model

--- components/atoms/renderMeal/renderMeal.models.ts
+++ components/atoms/renderMeal/renderMeal.models.ts
@@ -1,5 +1,13 @@
 import { Meal } from '@/classes';
+import { ScreenNames } from '@/constants';
+import { IRootStackParamList } from '@/models';
+import { NativeStackNavigationProp } from '@react-navigation/native-stack';
 
 type IRenderMeal = React.FC<Meal>;
 
-export { IRenderMeal };
+type INavigationProp = NativeStackNavigationProp<
+  IRootStackParamList,
+  ScreenNames.mealsDetail
+>;
+
+export { IRenderMeal, INavigationProp };
