4b3132ebf2a55c0255328ce6d04e4a03dde10727
Milza <marcomilza@gmail.com>
2025-06-11 23:32:04 +0200

    initialized drawer

--- Root/models.ts
+++ Root/models.ts
@@ -1,6 +1,8 @@
-import { IRootStackParamList } from '@/models';
+import { IDrawerStackParamList, IRootStackParamList } from '@/models';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import { createStackNavigator } from '@react-navigation/stack';
 
 const RootStack = createStackNavigator<IRootStackParamList>();
+const Drawer = createDrawerNavigator<IDrawerStackParamList>();
 
-export { RootStack };
+export { RootStack, Drawer };
